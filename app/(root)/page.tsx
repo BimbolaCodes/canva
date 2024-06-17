30e49db901a2c2363a20becfb8bb432a1a7a31f5
BimbolaCodes <bashorunbimbola@gmail.com>
2024-06-17 15:28:37 -0700

    clark auth added 70%

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,15 @@
+import { UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 const Home = () => {
   return (
     <div>
-      Home
+      <p>Home</p>
+      <UserButton />
     </div>
+
+    
   )
 }
 
-export default Home
+export default Home;
